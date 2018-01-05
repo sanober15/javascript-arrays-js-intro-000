92a585dc6c3ec5a06db322e44d10bdfb89a20951
sanober riaz <sanober@sanober.me>
2018-01-05 05:29:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -14,7 +14,7 @@ function destructivelyAddElementToBeginningOfArray(array, element){
 
 function addElementToEndOfArray(array, element){
   var n = array
-  n 
+  n
   n.push(element)
   return n;
 }

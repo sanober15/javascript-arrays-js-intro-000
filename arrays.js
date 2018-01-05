046b8b10e718eb7e841ff6cd17e11a986c2d0e3b
sanober riaz <sanober@sanober.me>
2018-01-05 05:25:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,13 +1,13 @@
 var chocolateBars = new Array();
 chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
-/*
+
 function addElementToBeginningOfArray(array, element){
-  var NewArray = [];
-  NewArray = [element, ...array];
+  var NewArray = []
+  NewArray = [element, ...array]
 
   return NewArray;
 }
-
+/*
 function destructivelyAddElementToBeginningOfArray(array, element){
  array;
  array = [element, ...array];
